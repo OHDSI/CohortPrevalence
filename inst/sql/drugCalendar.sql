d31eb744a868501c1028dfb16c22114cb274838a
Martin Lavallee <martin.lavallee@boehringer-ingelheim.com>
2026-09-25 16:01:15 -0400

    add current drug cal

--- inst/sql/drugCalendar.sql
+++ inst/sql/drugCalendar.sql
@@ -1,47 +1,53 @@
 
+
 DROP TABLE IF EXISTS #target_drug;
 CREATE TABLE #target_drug AS
 SELECT
-    t.cohort_definition_id AS target_id,
+    @prevalent_cohort_id AS target_id,
     t.subject_id,
     t.cohort_start_date AS target_start,
-    t.cohort_end_date AS target_end,
+    /*t.cohort_end_date AS target_end,*/
     t.span_label,
     t.calendar_start_date,
     t.calendar_end_date,
-    cc.codeset_id AS raw_occurrence_id,
-    d.drug_exposure_start_date AS event_date
+    d.cohort_definition_id AS drug_occurrence_id,
+    d.cohort_start_date AS drug_start_date,
+    d.cohort_end_date AS drug_end_date
 FROM (
   SELECT *
-  FROM #denom
-  WHERE cohort_definition_id IS NOT NULL
-  AND cohort_start_date BETWEEN DATEADD (day, -@lookBack, calendar_start_date) AND calendar_end_date
+  FROM #allEvents
+  WHERE case_event = 1
 ) t
-JOIN @cdm_database_schema.drug_exposure d ON t.subject_id = d.person_id
-JOIN #Codeset cc
-    ON d.drug_concept_id = cc.concept_id
-  WHERE t.calendar_start_date <= d.drug_exposure_start_date AND t.calendar_end_date >= d.drug_exposure_start_date
+JOIN (
+    SELECT *
+    FROM @work_database_schema.@cohort_table
+    WHERE cohort_definition_id IN (@drug_cohort_ids)
+)  d ON t.subject_id = d.subject_id
+JOIN @cdm_database_schema.observation_period op
+  ON t.subject_id = op.person_id
+  AND d.cohort_start_date <= op.observation_period_end_date
+  AND d.cohort_end_date >= op.observation_period_start_date
+WHERE t.calendar_start_date <= d.cohort_end_date AND t.calendar_end_date >= d.cohort_start_date
 ;
 
-
-
-/* Calculate the proportion of drug use per calendar year */
 DROP TABLE IF EXISTS #drug_cal_res;
 CREATE TABLE #drug_cal_res AS
 WITH T1 AS (
-  SELECT cohort_definition_id AS target_id, span_label, COUNT(DISTINCT subject_id) AS tot_year
-  FROM #denom
-  WHERE cohort_definition_id IS NOT NULL
-  AND cohort_start_date BETWEEN DATEADD (day, -@lookBack, calendar_start_date) AND calendar_end_date
-  GROUP BY cohort_definition_id, span_label
+  SELECT @prevalent_cohort_id as target_id, span_label, COUNT(DISTINCT subject_id) as tot_year
+  FROM #allEvents
+  WHERE case_event = 1
+  GROUP BY target_id, span_label
 ),
 T2 AS (
-    SELECT target_id, span_label, raw_occurrence_id, COUNT(DISTINCT subject_id) AS n_drug
-    FROM #target_drug
-    GROUP BY target_id, span_label, raw_occurrence_id
+  SELECT target_id, span_label, drug_occurrence_id,  COUNT(DISTINCT subject_id) AS n_drug
+  FROM #target_drug
+  GROUP BY target_id, span_label, drug_occurrence_id
 )
-SELECT a.target_id, a.span_label, b.raw_occurrence_id, b.n_drug, a.tot_year, (b.n_drug / a.tot_year) * 100 as pct
+SELECT
+    a.target_id, a.span_label, b.drug_occurrence_id,
+    b.n_drug, a.tot_year
 FROM T1 a
 JOIN T2 b ON a.target_id = b.target_id and a.span_label = b.span_label
-ORDER BY span_label, raw_occurrence_id
+ORDER BY span_label, drug_occurrence_id
 ;
+
